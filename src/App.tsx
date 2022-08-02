1dbc3d4352ac878c05806fa5a11c73ece964da38
Diogo Santos <diogodeese@gmail.com>
2022-08-02 22:49:01 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Keyboard from "./components/Keyboard";
+
+function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-slate-600">
+      <Keyboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Keyboard.tsx
@@ -0,0 +1,155 @@
+function keyboard() {
+  return (
+    <div className="flex flex-col">
+      <ul className="row row-0">
+        <li className="pinky" id="esc">
+          ESC
+        </li>
+        <li className="pinky" id="1">
+          1
+        </li>
+        <li className="ring" id="2">
+          2
+        </li>
+        <li className="middle" id="3">
+          3
+        </li>
+        <li className="pointer1st" id="4">
+          4
+        </li>
+        <li className="pointer2nd" id="5">
+          5
+        </li>
+        <li className="pointer2nd" id="6">
+          6
+        </li>
+        <li className="pointer1st" id="7">
+          7
+        </li>
+        <li className="middle" id="8">
+          8
+        </li>
+        <li className="ring" id="9">
+          9
+        </li>
+        <li className="pinky" id="10">
+          0
+        </li>
+        <li className="pinky">-</li>
+        <li className="pinky">+</li>
+        <li className="pinky w-20" id="back">
+          BACK
+        </li>
+      </ul>
+      <ul className="row row-1">
+        <li className="pinky w-20" id="tab">
+          TAB
+        </li>
+        <li className="pinky" id="Q">
+          Q
+        </li>
+        <li className="ring" id="W">
+          W
+        </li>
+        <li className="middle" id="E">
+          E
+        </li>
+        <li className="pointer1st" id="R">
+          R
+        </li>
+        <li className="pointer2nd" id="T">
+          T
+        </li>
+        <li className="pointer2nd" id="Y">
+          Y
+        </li>
+        <li className="pointer1st" id="U">
+          U
+        </li>
+        <li className="middle" id="I">
+          I
+        </li>
+        <li className="ring" id="O">
+          O
+        </li>
+        <li className="pinky" id="P">
+          P
+        </li>
+        <li className="pinky">[</li>
+        <li className="pinky">]</li>
+        <li className="pinky">\</li>
+      </ul>
+      <ul className="row row-2">
+        <li className="pinky w-24" id="caps">
+          CAPS
+        </li>
+        <li className="pinky" id="A">
+          A
+        </li>
+        <li className="ring" id="S">
+          S
+        </li>
+        <li className="middle" id="D">
+          D
+        </li>
+        <li className="pointer1st" id="F">
+          F
+        </li>
+        <li className="pointer2nd" id="G">
+          G
+        </li>
+        <li className="pointer2nd" id="H">
+          H
+        </li>
+        <li className="pointer1st" id="J">
+          J
+        </li>
+        <li className="middle" id="K">
+          K
+        </li>
+        <li className="ring" id="L">
+          L
+        </li>
+        <li className="pinky">:</li>
+        <li className="pinky">''</li>
+        <li className="pinky w-24" id="enter">
+          ENTER
+        </li>
+      </ul>
+      <ul className="row row-3">
+        <li className="pinky w-32" id="left-shift">
+          SHIFT
+        </li>
+        <li className="pinky" id="Z">
+          Z
+        </li>
+        <li className="ring" id="X">
+          X
+        </li>
+        <li className="middle" id="C">
+          C
+        </li>
+        <li className="pointer1st" id="V">
+          V
+        </li>
+        <li className="pointer2nd" id="B">
+          B
+        </li>
+        <li className="pointer2nd" id="N">
+          N
+        </li>
+        <li className="pointer1st" id="M">
+          M
+        </li>
+        <li className="middle">,</li>
+        <li className="ring">.</li>
+        <li className="pinky">;</li>
+        <li className="pinky w-32" id="right-shift">
+          SHIFT
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default keyboard;
