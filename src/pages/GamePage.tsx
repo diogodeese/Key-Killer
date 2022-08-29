f004b1178e2d57761a4407a8b8caf162286e38cb
Diogo Santos <diogodeese@gmail.com>
2022-08-29 22:59:39 +0100

    chore: create pages folder and game page

---
+++ src/pages/GamePage.tsx
@@ -0,0 +1,11 @@
+import Game from '../components/Game';
+
+function GamePage() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-slate-600">
+      <Game />
+    </div>
+  );
+}
+
+export default GamePage;
