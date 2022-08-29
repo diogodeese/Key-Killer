901ab9a0cb5083a2043cb5504a615dc307d51bd4
Diogo Santos <diogodeese@gmail.com>
2022-08-29 23:00:31 +0100

    chore: rename file

---
+++ src/components/Game.tsx
@@ -0,0 +1,137 @@
+import { useEffect, useRef } from 'react';
+import { keys, timer } from '../settings/settings.js';
+import Keyboard from './Keyboard.js';
+
+function getRandomNumber(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function selectRandomKey() {
+  document.querySelector('.selected')?.classList.remove('selected');
+  const randomKey = keys[getRandomNumber(0, keys.length - 1)];
+  const key = document.getElementById(randomKey);
+
+  if (key) {
+    key.classList.add('selected');
+  } else {
+    console.error('Key value is null');
+  }
+}
+
+function Game() {
+  const intervalRef = useRef(0);
+  let isPlaying: boolean = false;
+  let correctKeys: number = 0;
+  let wrongKeys: number = 0;
+  let time: number = timer * 60;
+
+  // Timer Interval
+  const interval = () => {
+    intervalRef.current = setInterval(() => {
+      updateTimer();
+    }, 1000);
+  };
+
+  function updateTimer() {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    const timerSpan = document.getElementById('timer');
+
+    if (timerSpan) {
+      timerSpan.innerText =
+        minutes.toString().padStart(2, '0') +
+        ':' +
+        seconds.toString().padStart(2, '0');
+    }
+
+    if (time <= 0) {
+      clearInterval(intervalRef.current);
+      endGame();
+      return;
+    }
+
+    time--;
+  }
+
+  function startGame() {
+    interval();
+    selectRandomKey();
+    isPlaying = true;
+  }
+
+  function endGame() {
+    document.querySelector('.selected')?.classList.remove('selected');
+    time = timer * 60;
+    console.log('Correct Keys: ' + correctKeys);
+    console.log('Wrong Keys: ' + wrongKeys);
+    correctKeys = 0;
+    wrongKeys = 0;
+    isPlaying = false;
+  }
+
+  useEffect(() => {
+    const handleKeyPress = (e: KeyboardEvent) => {
+      e.preventDefault();
+
+      // Pressed Key
+      const pressedKeyElement = document.getElementById(e.key.toUpperCase());
+      const pressedKey = e.key;
+
+      // Current Key
+      const currentKeyElement = document.querySelector('.selected');
+      const currentKey = currentKeyElement?.textContent?.toLowerCase();
+
+      // Key pressing animation
+      pressedKeyElement?.classList.add('hit');
+      pressedKeyElement?.addEventListener('animationend', () => {
+        pressedKeyElement?.classList.remove('hit');
+      });
+
+      if (pressedKey === currentKey) {
+        selectRandomKey();
+        correctKeys++;
+      } else if (pressedKey !== currentKey) {
+        wrongKeys++;
+      }
+    };
+
+    document.addEventListener('keypress', handleKeyPress);
+
+    const intervalId = intervalRef.current;
+
+    return () => {
+      clearInterval(intervalId);
+      document.removeEventListener('keypress', handleKeyPress);
+    };
+  }, []);
+
+  return (
+    <div>
+      <div className="flex justify-center items-center pb-8">
+        <span
+          className="timer"
+          id="timer"
+        >
+          00:00
+        </span>
+      </div>
+
+      <Keyboard />
+
+      <div className="flex justify-center items-center pt-8">
+        <button
+          className="start-button"
+          onClick={() => {
+            startGame();
+          }}
+          disabled={isPlaying}
+        >
+          Start Game
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Game;
--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,441 +1,282 @@
-import { useEffect, useRef } from 'react';
-
-const keys = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z',
-];
-
-function getRandomNumber(min: number, max: number) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-function selectRandomKey() {
-  document.querySelector('.selected')?.classList.remove('selected');
-  const randomKey = keys[getRandomNumber(0, keys.length - 1)];
-  const key = document.getElementById(randomKey);
-
-  if (key) {
-    key.classList.add('selected');
-  } else {
-    console.error('Key value is null');
-  }
-}
-
-function keyboard() {
-  const intervalRef = useRef(0);
-  let isPlaying: boolean = false;
-  let correctKeys: number = 0;
-  let wrongKeys: number = 0;
-  const timer: number = 0.2;
-  let time: number = timer * 60;
-
-  // Timer Interval
-  const interval = () => {
-    intervalRef.current = setInterval(() => {
-      updateTimer();
-    }, 1000);
-  };
-
-  function updateTimer() {
-    let minutes = Math.floor(time / 60);
-    let seconds = time % 60;
-
-    const timerSpan = document.getElementById('timer');
-
-    if (timerSpan) {
-      timerSpan.innerText =
-        minutes.toString().padStart(2, '0') +
-        ':' +
-        seconds.toString().padStart(2, '0');
-    }
-
-    if (time <= 0) {
-      clearInterval(intervalRef.current);
-      endGame();
-      return;
-    }
-
-    time--;
-  }
-
-  function startGame() {
-    interval();
-    selectRandomKey();
-    isPlaying = true;
-  }
-
-  function endGame() {
-    document.querySelector('.selected')?.classList.remove('selected');
-    time = timer * 60;
-    console.log('Correct Keys: ' + correctKeys);
-    console.log('Wrong Keys: ' + wrongKeys);
-    correctKeys = 0;
-    wrongKeys = 0;
-    isPlaying = false;
-  }
-
-  useEffect(() => {
-    const handleKeyPress = (e: KeyboardEvent) => {
-      e.preventDefault();
-
-      // Pressed Key
-      const pressedKeyElement = document.getElementById(e.key.toUpperCase());
-      const pressedKey = e.key;
-
-      // Current Key
-      const currentKeyElement = document.querySelector('.selected');
-      const currentKey = currentKeyElement?.textContent?.toLowerCase();
-
-      // Key pressing animation
-      pressedKeyElement?.classList.add('hit');
-      pressedKeyElement?.addEventListener('animationend', () => {
-        pressedKeyElement?.classList.remove('hit');
-      });
-
-      if (pressedKey === currentKey) {
-        selectRandomKey();
-        correctKeys++;
-      } else if (pressedKey !== currentKey) {
-        wrongKeys++;
-      }
-    };
-
-    document.addEventListener('keypress', handleKeyPress);
-
-    const intervalId = intervalRef.current;
-
-    return () => {
-      clearInterval(intervalId);
-      document.removeEventListener('keypress', handleKeyPress);
-    };
-  }, []);
-
+export default function Keyboard() {
   return (
-    <div>
-      <div className="flex justify-center items-center pb-8">
-        <span
-          className="timer"
-          id="timer"
+    <div className="flex flex-col border-slate-300 border-2 rounded p-2">
+      <ul className="row-0">
+        <li
+          className="pinky"
+          id="esc"
         >
-          00:00
-        </span>
-      </div>
-
-      <div className="flex flex-col border-slate-300 border-2 rounded p-2">
-        <ul className="row-0">
-          <li
-            className="pinky"
-            id="esc"
-          >
-            ESC
-          </li>
-          <li
-            className="pinky"
-            id="1"
-          >
-            1
-          </li>
-          <li
-            className="ring"
-            id="2"
-          >
-            2
-          </li>
-          <li
-            className="middle"
-            id="3"
-          >
-            3
-          </li>
-          <li
-            className="pointer1st"
-            id="4"
-          >
-            4
-          </li>
-          <li
-            className="pointer2nd"
-            id="5"
-          >
-            5
-          </li>
-          <li
-            className="pointer2nd"
-            id="6"
-          >
-            6
-          </li>
-          <li
-            className="pointer1st"
-            id="7"
-          >
-            7
-          </li>
-          <li
-            className="middle"
-            id="8"
-          >
-            8
-          </li>
-          <li
-            className="ring"
-            id="9"
-          >
-            9
-          </li>
-          <li
-            className="pinky"
-            id="10"
-          >
-            0
-          </li>
-          <li className="pinky">-</li>
-          <li className="pinky">+</li>
-          <li
-            className="pinky w-20"
-            id="back"
-          >
-            BACK
-          </li>
-        </ul>
-        <ul className="row-1">
-          <li
-            className="pinky w-20"
-            id="tab"
-          >
-            TAB
-          </li>
-          <li
-            className="pinky"
-            id="Q"
-          >
-            Q
-          </li>
-          <li
-            className="ring"
-            id="W"
-          >
-            W
-          </li>
-          <li
-            className="middle"
-            id="E"
-          >
-            E
-          </li>
-          <li
-            className="pointer1st"
-            id="R"
-          >
-            R
-          </li>
-          <li
-            className="pointer2nd"
-            id="T"
-          >
-            T
-          </li>
-          <li
-            className="pointer2nd"
-            id="Y"
-          >
-            Y
-          </li>
-          <li
-            className="pointer1st"
-            id="U"
-          >
-            U
-          </li>
-          <li
-            className="middle"
-            id="I"
-          >
-            I
-          </li>
-          <li
-            className="ring"
-            id="O"
-          >
-            O
-          </li>
-          <li
-            className="pinky"
-            id="P"
-          >
-            P
-          </li>
-          <li className="pinky">[</li>
-          <li className="pinky">]</li>
-          <li className="pinky">\</li>
-        </ul>
-        <ul className="row-2">
-          <li
-            className="pinky w-24"
-            id="caps"
-          >
-            CAPS
-          </li>
-          <li
-            className="pinky"
-            id="A"
-          >
-            A
-          </li>
-          <li
-            className="ring"
-            id="S"
-          >
-            S
-          </li>
-          <li
-            className="middle"
-            id="D"
-          >
-            D
-          </li>
-          <li
-            className="pointer1st"
-            id="F"
-          >
-            F
-          </li>
-          <li
-            className="pointer2nd"
-            id="G"
-          >
-            G
-          </li>
-          <li
-            className="pointer2nd"
-            id="H"
-          >
-            H
-          </li>
-          <li
-            className="pointer1st"
-            id="J"
-          >
-            J
-          </li>
-          <li
-            className="middle"
-            id="K"
-          >
-            K
-          </li>
-          <li
-            className="ring"
-            id="L"
-          >
-            L
-          </li>
-          <li className="pinky">:</li>
-          <li className="pinky">''</li>
-          <li
-            className="pinky w-24"
-            id="enter"
-          >
-            ENTER
-          </li>
-        </ul>
-        <ul className="row-3">
-          <li
-            className="pinky w-32"
-            id="left-shift"
-          >
-            SHIFT
-          </li>
-          <li
-            className="pinky"
-            id="Z"
-          >
-            Z
-          </li>
-          <li
-            className="ring"
-            id="X"
-          >
-            X
-          </li>
-          <li
-            className="middle"
-            id="C"
-          >
-            C
-          </li>
-          <li
-            className="pointer1st"
-            id="V"
-          >
-            V
-          </li>
-          <li
-            className="pointer2nd"
-            id="B"
-          >
-            B
-          </li>
-          <li
-            className="pointer2nd"
-            id="N"
-          >
-            N
-          </li>
-          <li
-            className="pointer1st"
-            id="M"
-          >
-            M
-          </li>
-          <li className="middle">,</li>
-          <li className="ring">.</li>
-          <li className="pinky">;</li>
-          <li
-            className="pinky w-32"
-            id="right-shift"
-          >
-            SHIFT
-          </li>
-        </ul>
-      </div>
-      <div className="flex justify-center items-center pt-8">
-        <button
-          className="start-button"
-          onClick={() => {
-            startGame();
-          }}
-          disabled={isPlaying}
+          ESC
+        </li>
+        <li
+          className="pinky"
+          id="1"
         >
-          Start Game
-        </button>
-      </div>
+          1
+        </li>
+        <li
+          className="ring"
+          id="2"
+        >
+          2
+        </li>
+        <li
+          className="middle"
+          id="3"
+        >
+          3
+        </li>
+        <li
+          className="pointer1st"
+          id="4"
+        >
+          4
+        </li>
+        <li
+          className="pointer2nd"
+          id="5"
+        >
+          5
+        </li>
+        <li
+          className="pointer2nd"
+          id="6"
+        >
+          6
+        </li>
+        <li
+          className="pointer1st"
+          id="7"
+        >
+          7
+        </li>
+        <li
+          className="middle"
+          id="8"
+        >
+          8
+        </li>
+        <li
+          className="ring"
+          id="9"
+        >
+          9
+        </li>
+        <li
+          className="pinky"
+          id="10"
+        >
+          0
+        </li>
+        <li className="pinky">-</li>
+        <li className="pinky">+</li>
+        <li
+          className="pinky w-20"
+          id="back"
+        >
+          BACK
+        </li>
+      </ul>
+      <ul className="row-1">
+        <li
+          className="pinky w-20"
+          id="tab"
+        >
+          TAB
+        </li>
+        <li
+          className="pinky"
+          id="Q"
+        >
+          Q
+        </li>
+        <li
+          className="ring"
+          id="W"
+        >
+          W
+        </li>
+        <li
+          className="middle"
+          id="E"
+        >
+          E
+        </li>
+        <li
+          className="pointer1st"
+          id="R"
+        >
+          R
+        </li>
+        <li
+          className="pointer2nd"
+          id="T"
+        >
+          T
+        </li>
+        <li
+          className="pointer2nd"
+          id="Y"
+        >
+          Y
+        </li>
+        <li
+          className="pointer1st"
+          id="U"
+        >
+          U
+        </li>
+        <li
+          className="middle"
+          id="I"
+        >
+          I
+        </li>
+        <li
+          className="ring"
+          id="O"
+        >
+          O
+        </li>
+        <li
+          className="pinky"
+          id="P"
+        >
+          P
+        </li>
+        <li className="pinky">[</li>
+        <li className="pinky">]</li>
+        <li className="pinky">\</li>
+      </ul>
+      <ul className="row-2">
+        <li
+          className="pinky w-24"
+          id="caps"
+        >
+          CAPS
+        </li>
+        <li
+          className="pinky"
+          id="A"
+        >
+          A
+        </li>
+        <li
+          className="ring"
+          id="S"
+        >
+          S
+        </li>
+        <li
+          className="middle"
+          id="D"
+        >
+          D
+        </li>
+        <li
+          className="pointer1st"
+          id="F"
+        >
+          F
+        </li>
+        <li
+          className="pointer2nd"
+          id="G"
+        >
+          G
+        </li>
+        <li
+          className="pointer2nd"
+          id="H"
+        >
+          H
+        </li>
+        <li
+          className="pointer1st"
+          id="J"
+        >
+          J
+        </li>
+        <li
+          className="middle"
+          id="K"
+        >
+          K
+        </li>
+        <li
+          className="ring"
+          id="L"
+        >
+          L
+        </li>
+        <li className="pinky">:</li>
+        <li className="pinky">''</li>
+        <li
+          className="pinky w-24"
+          id="enter"
+        >
+          ENTER
+        </li>
+      </ul>
+      <ul className="row-3">
+        <li
+          className="pinky w-32"
+          id="left-shift"
+        >
+          SHIFT
+        </li>
+        <li
+          className="pinky"
+          id="Z"
+        >
+          Z
+        </li>
+        <li
+          className="ring"
+          id="X"
+        >
+          X
+        </li>
+        <li
+          className="middle"
+          id="C"
+        >
+          C
+        </li>
+        <li
+          className="pointer1st"
+          id="V"
+        >
+          V
+        </li>
+        <li
+          className="pointer2nd"
+          id="B"
+        >
+          B
+        </li>
+        <li
+          className="pointer2nd"
+          id="N"
+        >
+          N
+        </li>
+        <li
+          className="pointer1st"
+          id="M"
+        >
+          M
+        </li>
+        <li className="middle">,</li>
+        <li className="ring">.</li>
+        <li className="pinky">;</li>
+        <li
+          className="pinky w-32"
+          id="right-shift"
+        >
+          SHIFT
+        </li>
+      </ul>
     </div>
   );
 }
-
-export default keyboard;
