290363aee54dd2c7112c9ac61ba917ba0d8fbe85
Diogo Santos <diogodeese@gmail.com>
2022-08-30 20:26:19 +0100

    feat: mobile handler

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,13 +1,37 @@
 import Nav from '../components/Nav';
 import Game from '../components/Game';
 
+function detectMob() {
+  const toMatch = [
+    /Android/i,
+    /webOS/i,
+    /iPhone/i,
+    /iPad/i,
+    /iPod/i,
+    /BlackBerry/i,
+    /Windows Phone/i,
+  ];
+
+  return toMatch.some((toMatchItem) => {
+    return navigator.userAgent.match(toMatchItem);
+  });
+}
+
 function GamePage() {
+  const isMobile = detectMob();
+
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center bg-slate-600">
-      <div className="absolute top-8">
-        <Nav />
-      </div>
-      <Game />
+      {!isMobile ? (
+        <>
+          <div className="absolute top-8">
+            <Nav />
+          </div>
+          <Game />
+        </>
+      ) : (
+        <div>Key Killer is only playable on a computer with a keyboard</div>
+      )}
     </div>
   );
 }
