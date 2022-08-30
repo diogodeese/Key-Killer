53b896e9397e57254c744cf0f6f306dcb6bd5aa0
Diogo Santos <diogodeese@gmail.com>
2022-08-30 01:59:44 +0100

    chore: import nav

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,8 +1,12 @@
+import Nav from '../components/Nav';
 import Game from '../components/Game';
 
 function GamePage() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-slate-600">
+    <div className="w-screen h-screen flex flex-col justify-center items-center bg-slate-600">
+      <div className="absolute top-8">
+        <Nav />
+      </div>
       <Game />
     </div>
   );
