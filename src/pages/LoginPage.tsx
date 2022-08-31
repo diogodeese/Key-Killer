7d30605b5af3944e98360739b1e7e01f89fc2f5a
Diogo Santos <53911973+diogodeese@users.noreply.github.com>
2022-08-31 23:35:11 +0100

    style: login page styling

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+
 function LoginPage() {
+  const navigate = useNavigate();
+
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center bg-slate-600">
-      Login Page
+      <div className="border-2 border-slate-600 rounded-xl p-8 translate-y-[-40%] bg-slate-400 min-w-[450px] min-h-[350px] shadow-lg flex flex-col justify-center items-center">
+        <p className="absolute top-8 text-xl">
+          Log in to compete on the ranking table!
+        </p>
+        Google Login Button
+        <button
+          className="button danger absolute bottom-8 w-full"
+          onClick={() => {
+            navigate('/');
+          }}
+        >
+          Return
+        </button>
+      </div>
     </div>
   );
 }
