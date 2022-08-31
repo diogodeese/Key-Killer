c6bf55055e472245619778af40bd98968c9dd928
Diogo Santos <53911973+diogodeese@users.noreply.github.com>
2022-08-31 23:51:30 +0100

    styles: ranking table styling

--- src/components/Ranking.tsx
+++ src/components/Ranking.tsx
@@ -1,41 +1,49 @@
 function Ranking() {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Rank</th>
-          <th>Player</th>
-          <th>Score</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Maria Anders</td>
-          <td>15</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Francisco Chang</td>
-          <td>14</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>Roland Mendel</td>
-          <td>13</td>
-        </tr>
-        <tr>
-          <td>4</td>
-          <td>Helen Bennett</td>
-          <td>11</td>
-        </tr>
-        <tr>
-          <td>5</td>
-          <td>Yoshi Tannamuri</td>
-          <td>9</td>
-        </tr>
-      </tbody>
-    </table>
+    <div className="w-screen flex items-center justify-center">
+      <table className="min-w-[60%]">
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Player</th>
+            <th>Correct Keys</th>
+            <th>Wrong Keys</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Maria </td>
+            <td>15</td>
+            <td>15</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Francisco Chang</td>
+            <td>14</td>
+            <td>15</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Roland Mendel</td>
+            <td>13</td>
+            <td>15</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>Helen Bennett</td>
+            <td>11</td>
+            <td>15</td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>Ola </td>
+            <td>9</td>
+            <td>15</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
 
