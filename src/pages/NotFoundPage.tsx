c462cfe526758a2fe28d5cc823144829d65ca3ab
Diogo Santos <53911973+diogodeese@users.noreply.github.com>
2022-10-16 22:44:27 +0100

    fix: not found page fix

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,7 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+
 function NotFoundPage() {
+  const navigate = useNavigate();
+
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center bg-slate-600">
-      404 Page Not Found
+      <span className="text-5xl font-semibold text-white pb-4 tracking-wider">
+        404
+      </span>
+      <span className="text-xl font-semibold text-white pb-8">
+        Page Not Found
+      </span>
+
+      <button
+        className="button"
+        onClick={() => navigate('/')}
+      >
+        Go Back
+      </button>
     </div>
   );
 }
